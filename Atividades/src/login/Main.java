128b996dbaed193b53f01bcf501f96c79fd1f4d5
Nelkisa Matias <gabrielamatias60@gmail.com>
2018-10-24 12:44:25 -0300

    adicionando um while para repetir até o user digitar o login/senha correto

--- Atividades/src/login/Main.java
+++ Atividades/src/login/Main.java
@@ -7,23 +7,30 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		
 		String loginV, login, senhaV, senha;
+		int saida = 1;
 		
 		System.out.println("Login: ");
 		loginV = sc.nextLine();
 		System.out.println("Senha: ");
 		senhaV = sc.nextLine();
 		
-		System.out.println("Insira o Login: ");
-		login = sc.nextLine();
-		System.out.println("Insira a Senha: ");
-		senha = sc.nextLine();
-		
-		if (login.equals(loginV) && senha.equals(senhaV)) {
-			System.out.println("é o bixão mesmo hein?");
-		}
-		else {
-			System.out.println("usuário ou senha incorretos!");
-		}
+		while (saida == 1) {
+						
+			System.out.println("Insira o Login: ");
+			login = sc.nextLine();
+			System.out.println("Insira a Senha: ");
+			senha = sc.nextLine();
+			
+			if (login.equals(loginV) && senha.equals(senhaV)) {
+				System.out.println("é o bixão mesmo hein?");
+				saida = 0;
+			}
+			else {
+				System.out.println("usuário ou senha incorretos!\n");
+				saida = 1;
+			}
+			
+		} 
 		
 	}
 
