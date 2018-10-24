50f29d4a06ba62055c3cfd15ff59298896b08052
Nelkisa Matias <gabrielamatias60@gmail.com>
2018-10-24 00:47:42 -0300

    pasta do projeto criada

---
+++ Atividades/src/login/Main.java
@@ -0,0 +1,30 @@
+package login;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String loginV, login, senhaV, senha;
+		
+		System.out.println("Login: ");
+		loginV = sc.nextLine();
+		System.out.println("Senha: ");
+		senhaV = sc.nextLine();
+		
+		System.out.println("Insira o Login: ");
+		login = sc.nextLine();
+		System.out.println("Insira a Senha: ");
+		senha = sc.nextLine();
+		
+		if (login.equals(loginV) && senha.equals(senhaV)) {
+			System.out.println("é o bixão mesmo hein?");
+		}
+		else {
+			System.out.println("usuário ou senha incorretos!");
+		}
+		
+	}
+
+}
